e8ae7488378ff63e2799daa67f50e71cb8c40f16
MaximGoal <golovmxm@gmail.com>
2021-07-07 22:15:46 +0300

    CrudMealRepository implement

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,32 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Modifying
+    @Query("UPDATE Meal m set m.user=:userId where m.id=:id")
+    Meal save(@Param("id") Integer id, @Param("userId") Integer userId);
+
+    List<Meal> findByUserIdOrderByDateTimeDesc(Integer userId);
+
+    Meal findByIdAndUserId(Integer id, Integer userId);
+
+    @Modifying
+    Integer deleteByIdAndUserId(Integer id, Integer userId);
+
+    @Query("SELECT m FROM Meal m WHERE m.user.id=:userId  AND m.dateTime >= :start AND m.dateTime < :end ORDER BY m.dateTime DESC")
+    List<Meal> getBetweenHalfOpen(
+            @Param("start") LocalDateTime startDateTime,
+            @Param("end") LocalDateTime endDateTime,
+            @Param("userId") Integer userId);
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,9 +1,12 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -11,33 +14,57 @@ import java.util.List;
 public class DataJpaMealRepository implements MealRepository {
 
     private final CrudMealRepository crudRepository;
+    private final CrudUserRepository crudUserRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
+    public DataJpaMealRepository(CrudMealRepository crudRepository, CrudUserRepository crudUserRepository) {
         this.crudRepository = crudRepository;
+        this.crudUserRepository = crudUserRepository;
     }
 
     @Override
+    @Transactional
+    @Modifying
     public Meal save(Meal meal, int userId) {
-        return null;
+
+        if (meal.getId() != null) {
+            User u = crudRepository.getById(meal.getId()).getUser();
+            if (u != null && u.id() != userId) {
+                return null;
+            }
+        }
+
+        if (meal.getUser() == null) {
+            User user = crudUserRepository.getById(userId);
+            meal.setUser(user);
+        }
+
+        Meal saved = crudRepository.save(meal);
+        if (meal.isNew()) {
+            Integer id = saved.getId();
+            meal.setId(id);
+        }
+        return saved;
     }
 
     @Override
+    @Transactional
+    @Modifying
     public boolean delete(int id, int userId) {
-        return false;
+            return crudRepository.deleteByIdAndUserId(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudRepository.findByIdAndUserId(id, userId);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.findByUserIdOrderByDateTimeDesc(userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return crudRepository.getBetweenHalfOpen(startDateTime, endDateTime, userId);
     }
 }
